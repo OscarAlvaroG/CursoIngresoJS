a57c3eb0575892178d90bb14b9ca8691eb0b5da5
OscarAlvaroG <oscar_alvarogarcia@hotmail.com>
2017-07-08 00:42:36 -0300

    adivina 1 y piedra
    terminado hasta piedra papel o tijera 1

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -9,23 +9,89 @@
 Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
-var numeroSecreto; 
-var contadorIntentos;
+var numeroSecreto = 0; 
+var intentos = 0;
+var temporzador;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
+	numeroSecreto = Math.floor(( Math.random() *100) + 1 );
+	console.log(numeroSecreto);
+	intentos = 0;
 
+	temporizador = setTimeout(verificar, 4000);// llama a funcion mensaje() despes de 3 segundos
+	resetear = setTimeout(reset,4000); // reseteo el juego a los 4 segundos 
 
-	 
-	
-
+}
+// resetea el juego
+function reset(){
+	numeroSecreto = 0;
+	intentos = 0;
 }
 
 function verificar()
 {
-	//verifico si intentos=1, =2 , =3, =4, =5,  >=6 && <10  , >=10	
-	
-	
+	// no comienza el juego si numero secreto es 0
+	if(numeroSecreto==0){
+		alert('La partida Termino, hace clic en comenzar! :)')
+	} else {
+		n_usuario = document.getElementById('numero').value;
+		//evalua si el numero secreto es igual al ingresado y la cantidad de intentos
+		intentos = intentos +1;
+		if (n_usuario==numeroSecreto && intentos == 1){
+				alert('Ganaste!, en '+intentos +" Intentos, Sos un Psiquico! :P");
+				document.getElementById('intentos').value = "Ganaste despues de "+intentos+" Intentos";
+				intentos = 0;
 
+			} else if (n_usuario == numeroSecreto && intentos == 2 ){
+				alert('Ganaste!, en '+intentos +" Intentos, Excelente Percepcion");
+				document.getElementById('intentos').value = "Ganaste despues de "+intentos+" Intentos";
+				intentos = 0;
+
+			} else if (n_usuario == numeroSecreto && intentos == 3){
+				alert('Ganaste!, en '+intentos +" Intentos, Esto es Suerte");
+				document.getElementById('intentos').value = "Ganaste despues de "+intentos+" Intentos";
+				intentos = 0;
+
+			} else if( n_usuario == numeroSecreto && intentos == 4){
+				alert('Ganaste!, en '+intentos +" Intentos, Excelente Tecnica");
+				document.getElementById('intentos').value = "Ganaste despues de "+intentos+" Intentos";
+				intentos = 0;
+
+			} else if ( n_usuario == numeroSecreto && intentos == 5){
+				alert('Ganaste!, en '+intentos +" Intentos, Estas en la Media ;)");
+				document.getElementById('intentos').value = "Ganaste despues de "+intentos+" Intentos";
+				intentos = 0;
+
+			} else if (n_usuario  == numeroSecreto && (intentos >=6 && intentos <= 10)){
+				alert('Ganaste!, en '+intentos +" Intentos, Te falta Tecnica :)");
+				document.getElementById('intentos').value = "Ganaste despues de "+intentos+" Intentos";
+				intentos = 0;
+
+			} else if (n_usuario == numeroSecreto && intentos > 10){
+				alert('Ganaste!, en '+intentos +" Intentos, Afortunado en el Amor :(");
+				document.getElementById('intentos').value = "Ganaste despues de "+intentos+" Intentos";
+				intentos = 0;
+			}
+				// si no se cumple lo anterior
+			else {
+				console.log("No hay coincidencias " + intentos + " intentos ");
+			}
+			// vrifico si me pase o no del numero secreto
+			if (n_usuario>numeroSecreto){
+				alert("Te pasaste del numero secreto");
+			} 
+			else if (n_usuario<numeroSecreto){
+				alert("Todavia falta para el numero secreto");
+			}
+			else{
+				
+			}
+			// finalizo el TimeOut
+			clearTimeout();
+
+	}//fin if numero secreto == 0
+   
 }
+
--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -4,26 +4,50 @@ siendo 1 para “piedra”, el 2 para “papel” y 3 para “tijera”.
 El jugador seleccionará una imagen correspondiente 
 a su opción  y le informaremos si ganó, empató o perdió.
 */
-var eleccionMaquina;
-
+var eleccionMaquina = 0;
 function comenzar()
 {
-	
-
+    //no encontre funcionalidad a comenzar, preguntar profe
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
+     eleccionMaquina = parseInt(Math.random()*3+1);
+     console.log(eleccionMaquina);
+    if (eleccionMaquina == 1 ){
+        console.log('Empatados! Piedra con Piedra!');
+    } else if (eleccionMaquina == 3 ){
+        console.log('Ganaste! Piedra Golpea a Tijera');
+    } else if (eleccionMaquina == 2 ){
+        console.log('Perdiste! Papel Envuelve a Piedra');
+    }
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
-
+     eleccionMaquina = parseInt(Math.random()*3+1);
+     console.log(eleccionMaquina);
+    if(eleccionMaquina == 1 ){
+        console.log('Ganaste! Papel Envuelve a Piedra');
+    } else if (eleccionMaquina == 2 ) {
+        console.log('Empate Papel con Papel');
+    } else if(eleccionMaquina == 3 ){
+        console.log('Perdiste Tijera Corta Papel');
+    }
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
+    eleccionMaquina = parseInt(Math.random()*3+1);
+    console.log(eleccionMaquina);
+
+    if(eleccionMaquina==1){
+        console.log('Perdiste, Piedra Golpea Tijera');
+    } else if( eleccionMaquina==2 ){
+        console.log('Ganaste Tijera Corta Papel');
+    } else if( eleccionMaquina==3){
+        console.log('Empatados! Tijera con Tijera');
+    }
 	
 
 }//FIN DE LA FUNCIÓN
